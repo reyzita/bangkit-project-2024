22b0dc11d2ff887ebf369429984b8fc09218a163
reyzita <89064851+reyzita@users.noreply.github.com>
2024-03-05 18:52:30 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit.logger import get_logger
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import seaborn as sns
 import requests
 from io import StringIO
